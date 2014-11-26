555efafcd4a4ad096ae87e3b7d4fe4e549188b53
tistatos <tistatos@gmail.com>
2014-11-26 13:56:05 +0100

    board can be arbitrary size, only left and up works

--- kod.py
+++ kod.py
@@ -1,6 +1,10 @@
 import random
 
 class Tile:
+    """
+    Tile Class
+    has a value and a position
+    """
     def __init__(self, posX, posY):
         self.value = 2 * random.randint(1,2)
         self.positionX = posX
@@ -8,42 +12,56 @@ class Tile:
         pass
 
 class Board:
+    """
+    Includes gamemechanics such as moving tiles and priting the play board
+    """
     def __init__(self):
-        self.size = (3,3)
-        self.tiles = [None]*9
+        """
+        Initalize board
+        """
+        self.sizeX = 4
+        self.sizeY = 4
+        self.tiles = [None]*(self.sizeX*self.sizeY)
         self.moveMade = False
         pass
+
     def start(self):
-        self.tiles = [None]*9
+        """
+        Reset board and generate random tile to start with
+        """
+        self.tiles = [None]*(self.sizeX*self.sizeY)
         self.generateTile()
         board.printBoard()
         pass
     def generateTile(self):
-        x = random.randint(0,2)
-        y = random.randint(0,2)
-        while self.tiles[x*3 + y] is not None:
-            x = random.randint(0,2)
-            y = random.randint(0,2)
+        """
+        generate a tile with either value 2 or 4 on empty position
+        """
+        y = random.randint(0,self.sizeY-1)
+        x = random.randint(0,self.sizeX-1)
+        while self.tiles[y*(self.sizeY) + x] is not None:
+            x = random.randint(0,self.sizeX-1)
+            y = random.randint(0,self.sizeY-1)
         tile = Tile(x,y)
-        self.tiles[tile.positionY*3 + tile.positionX] = tile
+        self.tiles[tile.positionY*self.sizeY +  tile.positionX] = tile
         pass
     def moveUp(self):
         for tile in self.tiles:
             if tile is not None:
                 if tile.positionY is not 0:
-                    tileAbove = self.tiles[(tile.positionY-1)*3 + tile.positionX]
+                    tileAbove = self.tiles[(tile.positionY-1)*self.sizeY + tile.positionX]
                     if tileAbove is None:
-                        self.tiles[(tile.positionY)*3 + tile.positionX] = None
+                        self.tiles[(tile.positionY)*self.sizeY + tile.positionX] = None
                         tile.positionY -= 1
-                        self.tiles[(tile.positionY)*3 + tile.positionX] = tile
+                        self.tiles[(tile.positionY)*self.sizeY + tile.positionX] = tile
                         self.moveMade = True
                         self.moveUp()
                         return
                     if tileAbove.value is tile.value:
-                        self.tiles[(tile.positionY)*3 + tile.positionX] = None
+                        self.tiles[(tile.positionY)*self.sizeY + tile.positionX] = None
                         tile.positionY -= 1
                         tile.value *= 2
-                        self.tiles[(tile.positionY)*3 + tile.positionX] = tile
+                        self.tiles[(tile.positionY)*self.sizeY + tile.positionX] = tile
                         self.moveMade = True
                         self.moveUp()
                         return
@@ -62,19 +80,19 @@ class Board:
         for tile in self.tiles:
             if tile is not None:
                 if tile.positionX is not 0:
-                    tileLeft = self.tiles[(tile.positionY)*3 + tile.positionX-1]
+                    tileLeft = self.tiles[(tile.positionY)*self.sizeY + tile.positionX-1]
                     if tileLeft is None:
-                        self.tiles[(tile.positionY)*3 + tile.positionX] = None
+                        self.tiles[(tile.positionY)*self.sizeY + tile.positionX] = None
                         tile.positionX -= 1
-                        self.tiles[(tile.positionY)*3 + tile.positionX] = tile
+                        self.tiles[(tile.positionY)*self.sizeY + tile.positionX] = tile
                         self.moveMade = True
                         self.moveLeft()
                         return
                     if tileLeft.value is tile.value:
-                        self.tiles[(tile.positionY)*3 + tile.positionX] = None
+                        self.tiles[(tile.positionY)*self.sizeY + tile.positionX] = None
                         tile.positionX -= 1
                         tile.value *= 2
-                        self.tiles[(tile.positionY)*3 + tile.positionX] = tile
+                        self.tiles[(tile.positionY)*self.sizeY + tile.positionX] = tile
                         self.moveMade = True
                         self.moveLeft()
                         return
@@ -87,7 +105,7 @@ class Board:
     def printBoard(self):
         newline = 0
         for tile in self.tiles:
-            if newline %3 is 0:
+            if newline %self.sizeY is 0:
                 print ''
             newline += 1
             if tile is None:
